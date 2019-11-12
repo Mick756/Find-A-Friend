bc9ff9a0568e6582c093e7269ea0e97eb5f4d90e
Mick Arias <mick756@gmail.com>
2019-11-12 11:22:14 -0700

    a push

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ Mongo.connect();
 require('./backend/authentication/loginAuth.js')(application);
 
 application.get('*', function (req, res) {
-    res.sendFile("index.html");
+    res.sendFile(__dirname + "/client/build/index.html");
 });
 
 application.listen(SERVER_PORT, () => {
