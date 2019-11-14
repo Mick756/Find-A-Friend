50bf55cc59fc0a3e5143ba7a4a2a9ae215b77455
Mick Arias <mick756@gmail.com>
2019-11-14 10:46:25 -0700

    might not work

---
+++ client/build/precache-manifest.1fa610e6a13b1e258797e308aa2f7d55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c5566e42cc0771ad4dfa6652049132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d45bfd947f4a672e0278",
+    "url": "/static/css/main.3fad297e.chunk.css"
+  },
+  {
+    "revision": "7743d3d5b027df9bb983",
+    "url": "/static/js/2.8bce3a46.chunk.js"
+  },
+  {
+    "revision": "d45bfd947f4a672e0278",
+    "url": "/static/js/main.79c84e5d.chunk.js"
+  },
+  {
+    "revision": "96d9506c0ec10f86d92f",
+    "url": "/static/js/runtime-main.a245edba.js"
+  }
+]);
